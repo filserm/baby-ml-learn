a2c8a1e8bf8abf95ea11b7a890e0c01c498e7ae5
filser michael <filser.michael@gmail.com>
2022-07-22 17:18:22 +0200

    changes

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM python:3.7-buster
+
+RUN apt-get update -y
+RUN apt-get install ffmpeg lame libatlas-base-dev alsa-utils -y
+RUN apt-get install pkg-config libhdf5-dev -y
+RUN apt-get install nano -y
+
+ENV PYTHONPATH=/app/micmon/
+
+RUN pip install https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0-rc2/tensorflow-2.4.0rc2-cp37-none-linux_aarch64.whl
+RUN pip install --upgrade numpy
+
+WORKDIR /app
+COPY . /app/
+
+ARG user=appuser
+ARG group=appuser
+ARG uid=1000
+ARG gid=1000
+RUN groupadd -g ${gid} ${group}
+RUN useradd -u ${uid} -g ${group} -s /bin/sh -m ${user} # 
+RUN chown -R ${uid}:${gid} /app
+RUN chmod 755 /app
+
+# Switch to user
+USER ${uid}:${gid}
+
+#RUN pip install -r /app/requirements.txt
+
+RUN git clone https://github.com:/BlackLight/micmon.git
+RUN pip install -r /app/micmon/requirements.txt
+
+USER root
+RUN python /app/micmon/setup.py build install
+
+USER appuser
+CMD [ "python", "/app/test.py"]
+
---
+++ model.py
@@ -0,0 +1,50 @@
+import os
+from tensorflow.keras import layers
+
+from micmon.dataset import Dataset
+from micmon.model import Model
+
+# This is a directory that contains the saved .npz dataset files
+datasets_dir = os.path.expanduser('~/by-ml/datasets/sound-detect/data')
+
+# This is the output directory where the model will be saved
+model_dir = os.path.expanduser('~//by-ml/models/sound-detect')
+
+# This is the number of training epochs for each dataset sample
+epochs = 2
+
+# Load the datasets from the compressed files.
+# 70% of the data points will be included in the training set,
+# 30% of the data points will be included in the evaluation set
+# and used to evaluate the performance of the model.
+datasets = Dataset.scan(datasets_dir, validation_split=0.3)
+labels = ['negative', 'positive']
+freq_bins = len(datasets[0].samples[0])
+
+# Create a network with 4 layers (one input layer, two intermediate layers and one output layer).
+# The first intermediate layer in this example will have twice the number of units as the number
+# of input units, while the second intermediate layer will have 75% of the number of
+# input units. We also specify the names for the labels and the low and high frequency range
+# used when sampling.
+model = Model(
+    [
+        layers.Input(shape=(freq_bins,)),
+        layers.Dense(int(2 * freq_bins), activation='relu'),
+        layers.Dense(int(0.75 * freq_bins), activation='relu'),
+        layers.Dense(len(labels), activation='softmax'),
+    ],
+    labels=labels,
+    low_freq=datasets[0].low_freq,
+    high_freq=datasets[0].high_freq
+)
+
+# Train the model
+for epoch in range(epochs):
+    for i, dataset in enumerate(datasets):
+        print(f'[epoch {epoch+1}/{epochs}] [audio sample {i+1}/{len(datasets)}]')
+        model.fit(dataset)
+        evaluation = model.evaluate(dataset)
+        print(f'Validation set loss and accuracy: {evaluation}')
+
+# Save the model
+model.save(model_dir, overwrite=True)
